9da8ce34c9d5cf967baa9d64905141c5d0f03e2a
DCartist <pbannerman13@gmail.com>
2021-01-20 17:03:09 -0500

    adding to index

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const serverless = require('serverless-http');
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-module.exports.handler = serverless(app);
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), () => {
+    console.log(`✅ PORT: ${app.get("port")} 🌟`);
+    console.log(`http://localhost:${app.get("port")}`)
+});
